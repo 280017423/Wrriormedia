fdd815e16d572d9f1548d01f82e4b6b9f038c2af
280017423 <zshq280017423@gmail.com>
2015-07-10 14:09:48 +0800

    only have picture ad, no video.

--- app/src/main/java/com/wrriormedia/app/business/manager/DownloadManager.java
+++ app/src/main/java/com/wrriormedia/app/business/manager/DownloadManager.java
@@ -5,6 +5,7 @@ import com.wrriormedia.app.business.dao.DBMgr;
 import com.wrriormedia.app.common.ConstantSet;
 import com.wrriormedia.app.model.DownloadModel;
 import com.wrriormedia.app.model.EventBusModel;
+import com.wrriormedia.app.model.MediaImageModel;
 import com.wrriormedia.app.model.MediaVideoModel;
 import com.wrriormedia.app.util.DownLoadUtil;
 import com.wrriormedia.library.eventbus.EventBus;
@@ -18,9 +19,13 @@ public class DownloadManager {
         List<DownloadModel> downloadModels = DBMgr.getBaseModels(DownloadModel.class, DownloadModel.IS_DOWNLOAD_FINISH + " = 0");
         if (null != downloadModels && !downloadModels.isEmpty()) {
             for (DownloadModel model : downloadModels) {
-                if (null != model && null != model.getVideo()) {
+                if (null != model) {
                     MediaVideoModel downLoadVideoModel = model.getVideo();
-                    if (StringUtil.isNullOrEmpty(downLoadVideoModel.getFileName())) {
+                    MediaImageModel downLoadImageModel = model.getImage();
+                    if (null == downLoadVideoModel || StringUtil.isNullOrEmpty(downLoadVideoModel.getFileName())) {
+                        if (downLoadImageModel != null && !StringUtil.isNullOrEmpty(downLoadImageModel.getMd5())) {
+                            EventBus.getDefault().post(new EventBusModel(ConstantSet.KEY_EVENT_ACTION_LOADER_IMAGE, downLoadImageModel));
+                        }
                         continue;
                     }
                     if (1 != model.getIsDownloadFinish()) {
